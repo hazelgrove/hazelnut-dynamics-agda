860a259e678a0a82dd7011fb55192380d98cfd4f
Ian Voysey <iev@cs.cmu.edu>
2018-02-08 16:03:05 -0500

    scraps

--- status.sh
+++ status.sh
@@ -4,6 +4,6 @@ echo "\`\`\`"
 echo "Status as of " `date`
 echo "todos:       " `grep -i todo *.agda | wc -l`
 echo "postulates:  " `grep -i postulate *.agda | wc -l`
-echo "red boxes:   " `grep -i red-box *.agda | wc -l`
+echo "red boxes:   " `grep -i 'red box' *.agda | wc -l`
 echo "bug @cyrus-: " `grep -i cyrus *.agda | wc -l`
 echo "\`\`\`"
