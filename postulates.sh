65720fdfeb00214f64b9f7a73846a64e02d8c987
Ian Voysey <iev@cs.cmu.edu>
2018-10-06 19:24:11 -0400

    adding another check

--- postulates.sh
+++ postulates.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 grep --color=auto -A 1 'postulate' *.agda
+
+echo "----------------------------------------------------------------"
+
+grep --color=auto "open import structural" *.agda
