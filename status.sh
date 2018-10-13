e2ba94c8050dd3e718366f7bea8f95cdd0d51019
Ian Voysey <iev@cs.cmu.edu>
2018-10-13 16:12:51 -0400

    scraps

--- status.sh
+++ status.sh
@@ -2,7 +2,7 @@
 echo
 echo "\`\`\`"
 echo "Status as of " `date`
-echo "todos:       " `grep -i todo *.agda | wc -l`
+echo "todos:       " `grep -i todo * | wc -l`
 echo "postulates:  " `grep -i postulate *.agda | wc -l`
 echo "red brackets:" `grep -i 'red brackets' *.agda | wc -l`
 echo "\`\`\`"
